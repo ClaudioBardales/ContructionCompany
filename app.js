b7c9e7582ef563c917962c8b62c780950aab2c4b
Claudio <claudio@Claudios-Mac.local>
2022-01-03 21:50:45 -0800

    Did HTML and CSS

---
+++ app.js
@@ -0,0 +1,12 @@
+/* Navigation Functionality */
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', () => {
+  console.log(navbarLinks.classList.toggle('active'));
+});
+
+/* Live Date */
+const date = document.getElementById('date');
+
+date.innerHTML = new Date().getFullYear();
