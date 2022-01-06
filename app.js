1cc4eeccda7e7e6ad5c9f45eb31ceff7ff0795ef
Claudio <claudio@Claudios-Mac.local>
2022-01-05 23:18:33 -0800

    Added Multi Language Support Per Customer

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 /* Navigation Functionality */
 const toggleButton = document.querySelector('.toggle-button');
-const navbarLinks = document.querySelector('.navbar-links');
+const nav = document.querySelector('.navbar-links');
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
+  nav.classList.toggle('active');
 });
 
 /* Live Date */
 const date = document.getElementById('date');
 
 date.innerHTML = new Date().getFullYear();
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: 'en',
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+    },
+    'google_translate_element'
+  );
+}
